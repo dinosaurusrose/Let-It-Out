d6c86a9021cc06cfb76964b16867f0f90d459698
40105775 <sallen29@qub.ac.uk>
2017-12-03 20:27:01 +0000

    1st upload
    First upload of the UI

---
+++ App1/AboutPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace App1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class AboutPage : Page
+    {
+        public AboutPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void SelfHelpBtnClick(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(selfHelp));
+        }
+
+        private void AboutBtnClick(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(AboutPage));
+        }
+
+        private void InstructionBtnClick(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(HelpPage));
+        }
+
+        private void InfoBtnClick(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(ImportantInfoPage));
+        }
+    }
+}
